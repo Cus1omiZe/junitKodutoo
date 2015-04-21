92c6c57915417c369c42ccbfb5d19ccf1d22f7fc
MasTaVi <bomba389@mail.ru>
2015-04-21 10:38:59 +0300

    Changes to code

--- src/SubscriptionTest.java
+++ src/SubscriptionTest.java
@@ -7,13 +7,16 @@ public class SubscriptionTest {
    public void test_returnEuro() {
       System.out.println("Test if pricePerMonth returns Euro...") ;
       Subscription S = new Subscription(200,2) ;
-      assertTrue(S.pricePerMonth() == 1.0) ;
+      assertEquals(1.0, S.pricePerMonth(), 0.01) ;
    }
 
    @Test
    public void test_roundUp() {
       System.out.println("Test if pricePerMonth rounds up correctly...") ;
-      Subscription S = new Subscription(200,3) ;
-      assertTrue(S.pricePerMonth() == 0.67) ;
+      Subscription S = new Subscription(200, 3);
+      assertEquals(0.67, S.pricePerMonth(), 0.01) ;
+
    }
 }
+//
+//java -cp junit-4.12.jar;hamcrest-core-1.3.jar;. org.junit.runner.JUnitCore SubscriptionTest
